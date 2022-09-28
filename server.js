ed8d25050a404d2bc8dfd5b65fd71a81da7d3d6e
phuochau <phuoc.hau1148@gmail.com>
2022-09-28 15:00:02 +0700

    cài đặt các handler truy xuất CSDL

---
+++ server.js
@@ -0,0 +1,20 @@
+const app = require("./app");
+const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
+
+async function startServer(){
+    try{
+        await MongoDB.connect(config.db.uri);
+        console.log("connected to the database!");
+
+        const PORT = config.app.port;
+        app.listen(PORT,()=>{
+            console.log('server is running on port ' +PORT);
+        });
+    } catch (error) {
+        consolog.log("cannot connect to the database!",error);
+        process.exit();
+    }
+}
+
+startServer();
